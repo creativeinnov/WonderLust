197080f1e1a1626752d806e62e0b152759086242
creativeinnov <mehtalaxmi1234@gmail.com>
2025-07-31 00:37:12 +0530

    Modified file strutures and routes(Express Route)

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
+const {listingSchema, reviewSchema} = require("./schema.js");
 const Review = require("./models/review.js");
 
+const listings = require("./routes/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -34,8 +35,8 @@ app.get("/", (req,res) => {
     res.send("Hi, I am root");
 });
 
-const validateListing = (req, res, next) => {
-    let {error} = listingSchema.validate(req.body);
+const validateReview = (req, res, next) => {
+    let {error} = reviewSchema.validate(req.body);
     if(error){
         let errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(400, errMsg);
@@ -44,71 +45,11 @@ const validateListing = (req, res, next) => {
     }
 }
 
-//Index Route
-app.get("/listings", wrapAsync(async (req, res) => {
-    // Listing.find({}).then(res => {
-    //     console.log(res);
-    // })
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
-}));
-
-//New Route
-app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs");
-});
-
-//Show Route
-app.get("/listings/:id", wrapAsync(async (req, res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
-}));
-
-//Create Route
-app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
-    // let {title, description, price, location, country} = req.body;
-    // let listing = req.body.listing;
-        // console.log(listing);
-        // if(!req.body.listing){
-        //     throw new ExpressError(400, "Send valid data for listing");
-        // }
-    // let result = listingSchema.validate(req.body);
-    // console.log(result);
-    // if(result.error){
-    //     throw new ExpressError(400, result.error);
-    // }
-    const newListing = new Listing(req.body.listing);
-
-    await newListing.save();
-    res.redirect("/listings"); 
-}));
-
-//Edit Route
-app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
-}));
-
-//Update Route
-app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect(`/listings/${id}`);
-}));
-
-//Delete Route
-app.delete("/listings/:id", wrapAsync(async (req, res) => {
-    let {id} = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}));
+app.use("/listings", listings);
 
 //Reviews
 //Post Route
-app.post("/listings/:id/reviews", async(req, res) => {
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
 
@@ -121,7 +62,16 @@ app.post("/listings/:id/reviews", async(req, res) => {
     res.redirect(`/listings/${listing._id}`)
    
     // res.send("new review saved");
-})
+}));
+
+//Delete Review Route
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
+    let {id, reviewId} = req.params;
+    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+}));
 
 
 // app.get("/testListing", async (req, res) => {
---
+++ classroom/routes/post.js
@@ -0,0 +1,26 @@
+//Posts
+//Index
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("GET for posts");
+});
+
+//Show
+router.get("/:id", (req, res) => {
+    res.send("GET for post id");
+});
+
+//POST
+router.post("/", (req, res) => {
+    res.send("POST for posts");
+});
+
+//DELETE
+router.delete("/:id", (req, res) => {
+    res.send("DELETE for post id");
+});
+
+module.exports = router;
